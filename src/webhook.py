a6ba66ccf7072843911f7ad26988b99b09078de0
Seth-R <56092001+Seth-R@users.noreply.github.com>
2021-04-14 14:24:36 -0600

    Update webhook.py

--- src/webhook.py
+++ src/webhook.py
@@ -1,5 +1,9 @@
-import request
+import requests
 import json
 
-webhook_url= 'http://zohopruebaapi.tk/'
+webhook_url= 'https://webhook.site/cc20fa17-b9e8-42bc-a957-9d2396b68ac2'
 
+data = { 'name': 'prueba', 
+         'info': 'quiubole' }
+
+r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
